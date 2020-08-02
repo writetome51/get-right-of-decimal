8bb07e177b0fe7db8e71c8a63eb5fe8b7337e9f6
Steve Thompson <writetome51@gmail.com>
2020-08-02 00:41:22 -0600

    updated to be ecmascript module. Committing version 2.0.0

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { errorIfNotFloat } from 'error-if-not-float';
+import {errorIfNotFloat} from 'error-if-not-float';
 
 
 // It's important this returns a string, mainly because it might return a set of
 // digits that begin with leading zeros, for instance: '005'.  If this were converted
 // to a number, Javascript would eliminate the leading zeros and return 5.
 
-export function getRightOfDecimal(num): string {
+export function getRightOfDecimal(num) {
 	errorIfNotFloat(num);
 	return (String(num).split('.')[1]);
 }
--- tests.js
+++ tests.js
@@ -1,5 +1,7 @@
-import { getRightOfDecimal } from './index';
+import { getRightOfDecimal } from './index.js';
+
 let nonFloats = [0, '', '1', '100', true, false, NaN, Number, Infinity, Number.MAX_VALUE, [], {}, String];
+
 // Test 1 : Must trigger error in each iteration:
 let i = -1, errorsTriggered = 0;
 while (++i < nonFloats.length) {
@@ -14,24 +16,32 @@ if (errorsTriggered === nonFloats.length)
     console.log('test 1 passed');
 else
     console.log('test 1 failed');
+
+
 // Test 2
 let result = getRightOfDecimal(0.000001);
 if (result === '000001')
     console.log('test 2 passed');
 else
     console.log('test 2 failed');
+
+
 // Test 3
 result = getRightOfDecimal(6.4000000001);
 if (result === '4000000001')
     console.log('test 3 passed');
 else
     console.log('test 3 failed');
+
+
 // Test 4
 result = getRightOfDecimal(-.4000000001);
 if (result === '4000000001')
     console.log('test 4 passed');
 else
     console.log('test 4 failed');
+
+
 // Test 5
 result = getRightOfDecimal(4199111111221.111);
 if (result === '111')
